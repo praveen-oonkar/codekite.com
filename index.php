82ac168f60029ef834b304f9afc4d390ded7d449
Narasimha Rao <raonarasimha050@gmail.com>
2014-12-20 17:55:23 +0530

    0.2

--- index.php
+++ index.php
@@ -3,8 +3,110 @@
 <head>
 	<meta charset="UTF-8">
 	<title>DashBoard</title>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script	src="js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/paper.css">
 </head>
 <body>
+	<!--Nav Bar code-->
+	<nav class="navbar navbar-inverse" role="navigation">
+  		<div class="container-fluid">
+  			<div class="navbar-header">
+  				<a class="navbar-brand" href="index.php">CodeKite</a>
+  			</div>
+
+  			<ul class="nav navbar-nav navbar-right">
+	  			<li class="dropdown">
+			         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">UserName <span class="caret"></span></a>
+			          <ul class="dropdown-menu" role="menu">
+			            <li><a href="#">Report Bugs!</a></li>
+			            <li class="divider"></li>
+			            <li><a href="#">Logout</a></li>
+			          </ul>
+	        	</li>
+  			</ul>
+  		</div>
+  	</nav>
 	
+	<!--Create new Code button-->
+	<!--<p class="col-md-2 col-md-offset-5">-->
+	<button class="btn btn-info btn-lg" style="margin-left:25px;">Create new <em>code page</em></button>
+	<br><br>
+	
+		
+			<table class="table table-striped table-bordered">
+				<thead>
+			  		<tr>
+			  			<th>CodePage Name</th>
+			  			<th>Updated</th>
+			  			<th>Created</th>
+			  			<th>Shared with</th>
+			  			
+			  		</tr>
+			  	</thead>
+			  	<tbody>
+			  		<tr>
+			  			<td>hell</td>
+			  			<td>d</td>
+			  			<td>dfsgd</td>
+			  			<td>dsdgsdg</td>
+			  			
+			  		</tr>
+			  		<tr>
+			  			<td>hell</td>
+			  			<td>d</td>
+			  			<td>dfsgd</td>
+			  			<td>dsdgsdg</td>
+			  			
+			  		</tr>
+			  		<tr>
+			  			<td>hell</td>
+			  			<td>d</td>
+			  			<td>dfsgd</td>
+			  			<td>dsdgsdg</td>
+			  			
+			  		</tr>
+	  			</tbody>
+			</table>
+		
+	
+	<table class="table table-striped table-bordered">
+				<thead>
+			  		<tr>
+			  			<th>Page Name</th>
+			  			<th>Updated</th>
+			  			<th>Created</th>
+			  			<th>Invited by</th>
+			  			
+			  		</tr>
+			  	</thead>
+			  	<tbody>
+			  		<tr>
+			  			<td>hellsdhskdgfklsdjfjaksdfgjanbfjgbjsafgbj</td>
+			  			<td>djvksdnfknsdkfskdgfksndkgfnskdngfklsdn</td>
+			  			<td>dfsdglskndfnskadnfkndlkngd</td>
+			  			<td>dsdgsvlksndkfnadskngfksdngfklsmdlkfnmakldg</td>
+			  			
+			  		</tr>
+			  		<tr>
+			  			<td>hell</td>
+			  			<td>d</td>
+			  			<td>dfsgd</td>
+			  			<td>dsdgsdg</td>
+			  			
+			  		</tr>
+			  		<tr>
+			  			<td>hell</td>
+			  			<td>d</td>
+			  			<td>dfsgd</td>
+			  			<td>dsdgsdg</td>
+			  			
+			  		</tr>
+	  			</tbody>
+			</table>
+
+		
+            
+        	
 </body>
 </html>
